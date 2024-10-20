47c13fa59fc76306abcb8ad7342a37ecd4bb3402
fendrick <its.fendrick@example.com>
2024-10-20 16:44:25 +0530

    fix linting

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,12 +7,10 @@ import { ModeToggle } from "@/components/ModeToggle";
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 
 import Link from "next/link";
