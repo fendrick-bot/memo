0ca4fd937097b6ea43276fc94efce7be8cd825cd
fendrick <its.fendrick@example.com>
2024-10-23 00:15:43 +0530

    fendrick_production check

--- components/AppLogo.tsx
+++ components/AppLogo.tsx
@@ -1,13 +1,12 @@
 import dark_logo from "@/public/dark-logo.svg";
-import light_logo from "@/public/light-logo.svg";
 import Image from "next/image";
 
 export function AppLogo(){
     const logo_size = 32;
     return (
         <div className="pr-1">
-            <Image src={dark_logo} alt="Memo Logo" width={logo_size} height={logo_size} className="dark:hidden"/>
-            <Image src={dark_logo} alt="Memo Logo" width={logo_size} height={logo_size} className="hidden dark:block" />
+            <Image src={dark_logo} alt="Memo Logo" width={logo_size} height={logo_size}/>
+          
         </div>
 
     )
